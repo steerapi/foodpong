79ebc20528d1d6a114a0c5f06ea80c18691d8cd3
Surat Teerapittayanon <steerapi@gmail.com>
2012-10-06 11:23:55 -0400

    add key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh vertx@107.21.250.18 -i ~/key.pem "cd ~/foodpong && git pull"
-ssh ubuntu@107.21.250.18 -i ~/key.pem sudo stop foodpong
-ssh ubuntu@107.21.250.18 -i ~/key.pem sudo start foodpong
+ssh vertx@107.21.250.18 -i key.pem "cd ~/foodpong && git pull"
+ssh ubuntu@107.21.250.18 -i key.pem sudo stop foodpong
+ssh ubuntu@107.21.250.18 -i key.pem sudo start foodpong
